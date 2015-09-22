4bca32ddc60a0934f2a5ebbd8ac32265787ea7dd
Jimmy Brisson <theotherjimmy@gmail.com>
2015-09-22 17:00:07 -0500

    removed stellarisware includes; used US spi

--- src/main.c
+++ src/main.c
@@ -3,12 +3,6 @@
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/spi.h>
 #include <RASLib/inc/time.h>
-#include <StellarisWare/driverlib/ssi.h>
-#include <StellarisWare/driverlib/gpio.h>
-#include <StellarisWare/driverlib/sysctl.h>
-#include <StellarisWare/inc/hw_ssi.h>
-#include <StellarisWare/inc/hw_types.h>
-#include <StellarisWare/inc/hw_memmap.h>
 #include <stdint.h>
 
 static int ledState = 0;
@@ -38,16 +32,16 @@ void reverse_array (uint32_t * array, int size){
 int send_packet_PS2(uint32_t * packet, int size, uint32_t * response) {
         int  buff_size = 3;
         SetPin(PIN_B2, 0);
-        SPIRequest(spi, -1, &packet[0], 3 , &response[0], 3, 0.000016f);
+        SPIRequestUS(spi, -1, &packet[0], 3 , &response[0], 3, 16);
         reverse_array (response, 3);
         buff_size += 2 * (response[mode_byte] & 0x0F);
-        SPIRequest(spi, -1, &packet[3], size - 3 , &response[3], buff_size - 3, 0.000016f);
+        SPIRequestUS(spi, -1, &packet[3], size - 3 , &response[3], buff_size - 3, 16);
         reverse_array (&response[3], buff_size - 3);
         SetPin(PIN_B2, 1);
         return buff_size;
 }
 
-#define d_buf_size (100)
+#define d_buf_size (37)
 uint32_t poll[3] = {0x01, 0x42, 0x00};
 uint32_t poll_and_escape[5] = {0x01, 0x43, 0x00, 0x01, 0x00};
 uint32_t set_dualshock_mode[9] = { 0x01, 0x44, 0x00, 0x01, 0x03
@@ -56,48 +50,52 @@ uint32_t set_analog_button_mode[9] = { 0x01, 0x4f, 0x00, 0xFF, 0xFF
                                        , 0x03, 0x00, 0x00, 0x00};
 uint32_t leave_escape[5] = {0x01, 0x43, 0x00, 0x00, 0x00};
 
+#define arraySizeof(arry) (sizeof(arry) / sizeof(arry[0]))
+
 int main (void) {
-        int i, buff_size;
         uint32_t data[d_buf_size] = {0x00,};
-        reverse_array(poll,3);
-        reverse_array(poll_and_escape,5);
-        reverse_array(set_dualshock_mode,9);
-        reverse_array(set_analog_button_mode,9);
-        reverse_array(leave_escape,5);
+        reverse_array(&poll[0],
+                      arraySizeof(poll));
+        reverse_array(&poll_and_escape[0],
+                      arraySizeof(poll_and_escape));
+        reverse_array(&set_dualshock_mode[0],
+                      arraySizeof(set_dualshock_mode));
+        reverse_array(&set_analog_button_mode[0],
+                      arraySizeof(set_analog_button_mode));
+        reverse_array(&leave_escape[0],
+                      arraySizeof(leave_escape));
         CallEvery(ToggleLED, 0, 0.25);
         spi = InitializeSPI(PIN_A2, PIN_A5, PIN_A4, 250000, 8, true, true);
         do {
-                buff_size = send_packet_PS2(&poll_and_escape[0], 5, &data[0]);
-                for (i = 0; i < buff_size; ++i) {
-                        Printf("0x%02x ", data[i]);
-                }
-                Printf("\n");
-                buff_size = send_packet_PS2(&set_dualshock_mode[0], 9, &data[0]);
-                for (i = 0; i < buff_size; ++i) {
-                        Printf("0x%02x ", data[i]);
-                }
-                Printf("\n");
-                buff_size = send_packet_PS2(&set_analog_button_mode[0], 9, &data[0]);
-                for (i = 0; i < buff_size; ++i) {
-                        Printf("0x%02x ", data[i]);
-                }
-                Printf("\n");
-                buff_size = send_packet_PS2(&leave_escape[0], 5, &data[0]);
-                for (i = 0; i < buff_size; ++i) {
-                        Printf("0x%02x ", data[i]);
-                }
-                Printf("\n");
-                buff_size = send_packet_PS2(&poll[0], 3, &data[0]);
-                for (i = 0; i < buff_size; ++i) {
-                        Printf("0x%02x ", data[i]);
-                }
-                Printf("\n");
+                send_packet_PS2(&poll_and_escape[0],
+                                arraySizeof(poll_and_escape), &data[0]);
+                Wait(0.01);
+                send_packet_PS2(&set_dualshock_mode[0],
+                                arraySizeof(set_dualshock_mode), &data[0]);
+                Wait(0.01);
+                send_packet_PS2(&set_analog_button_mode[0],
+                                arraySizeof(set_analog_button_mode), &data[0]);
+                Wait(0.01);
+                send_packet_PS2(&leave_escape[0],
+                                arraySizeof(leave_escape), &data[0]);
+                Wait(0.01);
+                send_packet_PS2(&poll[0],
+                                arraySizeof(poll), &data[0]);
         } while (data[1] != 0x79);
         while (1) {
-                buff_size = send_packet_PS2(&poll[0], 3, &data[0]);
-                for (i = 0; i < buff_size; ++i) {
-                        Printf("0x%02x ", data[i]);
-                }
-                Printf("\r");
+                int buff_size = send_packet_PS2(&poll[0],
+                                                arraySizeof(poll), &data[0]);
+                if (buff_size == 21)
+                        Printf("Lx %03d Ly %03d Rx %03d Ry %03d "
+                               "X %03d Square %03d Triangle %03d Circle %03d "
+                               "Up %03d Down %03d Left %03d Right %03d "
+                               "L1 %03d L2 %03d R1 %03d R2 %03d\r",
+                               data[7], data[8], data[5], data[6],
+                               data[15], data[16], data[13], data[14],
+                               data[11], data[12], data[10], data[9],
+                               data[17], data[19], data[18], data[20]);
+                else if (buff_size == 9)
+                        Printf("Lx %03d Ly %03d Rx %03d Ry %03d\r",
+                               data[7], data[8], data[5], data[6]);
         }
 }
