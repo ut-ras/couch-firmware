f7ecbf0a865ff4508dd36b05de46ef1154ca4c46
dteal <daniel@teals.org>
2017-04-04 08:48:08 -0500

    Added compilation output files to gitignore.

--- src/Main.d
+++
@@ -1,6 +0,0 @@
-Main.o: Main.c ../raslib/../raslib/inc/common.h \
- /usr/local/lib/StellarisWare/../StellarisWare/inc/hw_types.h \
- ../raslib/../raslib/inc/uart.h ../raslib/../raslib/inc/gpio.h \
- ../raslib/../raslib/inc/common.h ../raslib/../raslib/inc/time.h \
- ../raslib/../raslib/inc/pwm.h ../raslib/../raslib/inc/gpio.h \
- ../raslib/../raslib/inc/uart.h
